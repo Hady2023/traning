cf75bc8ce0d953e016dccf0bfa8696dea97e7368
Hady2023 <hadyayman899@gmail.com>
2025-11-24 12:56:08 +0200

    Bug fix from this form (BUG_fix/)

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 int main() {
   std::cout << "Hello"<< std::endl;
-  std::cout << "add new feature from version1" << std::endl;     
+  std::cout << "add new feature from version1" << std::endl;
+  std::cout << "add from release/ * "<< std::endl;       
   return 0;
 }
