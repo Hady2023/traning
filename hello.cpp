25a0e897540093464887ee415e6aa6c3a9d48a1f
Hady2023 <hadyayman899@gmail.com>
2025-11-24 13:52:52 +0200

    Create Successful Pull Request

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@ int main() {
   std::cout << "Hello"<< std::endl;
   std::cout << "add new feature from version1" << std::endl;
   std::cout << "add from release/ * "<< std::endl;
-  std::cout << "Add from Fix normal file" << std::endl;        
+  std::cout << "Add from Fix normal file" << std::endl;
+  std::cout << "Create Pull Request to mai" << std::endl;        
   return 0;
 }
